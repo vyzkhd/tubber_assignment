2bf17bdc00dfaae1a8ab090acb57bd8bd1c4c5d9
Vyshakh M <45941707+vyzkhd@users.noreply.github.com>
2021-04-04 12:34:23 +0530

    fix admin tasks

--- admin.py
+++ admin.py
@@ -1,5 +1,6 @@
 from models import Event
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from datetime import datetime, timedelta
 import logging
 
@@ -9,29 +10,30 @@ logging.basicConfig(filename='tubber.log', filemode='w', format='%(name)s - %(le
 def check_first_bill(db: Session, userid: int):
     counts = db.query(Event).filter(Event.userid == userid, Event.noun == "bill").count()
 
-    if counts > 0:
+    if counts == 1:
         logging.info(f"first bill pay for user {userid}")
+        return True 
+    else:
+        return False 
 
 
 def alert_user(db: Session, userid: int):
     now = datetime.now()
     five_mins_ago = now - timedelta(minutes=5)
 
-    nums_over_20000 = db.query(Event).filter(Event.userid == userid, Event.properties.value >= 20000, Event.ts > five_mins_ago).count()
-
-    if nums_over_20000 > 5:
+    #nums_over_20000 = db.query(Event).filter(Event.userid == userid, Event.properties.value >= 20000, Event.ts > five_mins_ago).count()
+    total = db.query(func.sum(Event.properties.value)).filter(Event.userid == userid, datetime.strptime(Event.ts, '%Y%m%d %H%M%S')  > five_mins_ago).order_by(datetime.strptime(Event.ts, '%Y%m%d %H%M%S') ).limit(5).all()
+    if total > 20000:
         logging.info(f"notifying user {userid}")
 
 def check_feedback(db: Session, userid: int):
-    
+      
+    bill_pay_time = datetime.now() - timedelta(minutes=15)
 
-    if db.query(Event).filter(Event.userid == userid, Event.noun == "bill").count() > 0:
-        bill_pay_event = db.query(Event).filter(Event.userid == userid, Event.noun == "bill").first().ts 
-        fifteen_mins_later = datetime.strptime(bill_pay_event, '%Y%m%d %H%M%S') + timedelta(minutes=15)
-        
-        if db.query(Event).filter(Event.userid == userid, Event.noun == "fdfk").count() > 0:
-            ts_fdbk = db.query(Event).filter(Event.userid == userid, Event.noun == "fdfk").first().ts
-            if ts_fdbk > fifteen_mins_later:
-                logging.info("notifying Tubber engineer")
-    
+    if db.query(Event).filter(Event.userid == userid, Event.noun == "fdfk").count() > 0:
+        ts_fdbk = db.query(Event).filter(Event.userid == userid, Event.noun == "fdfk").first().ts
+        fdbk_time = datetime.strptime(ts_fdbk, '%Y%m%d %H%M%S') 
+        if fdbk_time < bill_pay_time:
+            logging.info("notifying Tubber engineer")
+
       
